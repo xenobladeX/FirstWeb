9b11bf72ed086d0bee7e05cd920d9de06f1d6485
丁明 <xenonlade407@icloud.com>
2018-07-18 14:18:58 +0800

    complete html structure

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+
+### 0.0.1
+
+* crate a template for develop none-framework web application
+* main page for this web
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -3,15 +3,5 @@ import '../../js/bootstrap/index';
 import './index.scss';
 
 $(document).ready(function () {
-
-    $(document).on("click.test", "#left-well button", function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        console.log("event1: ", e);
-        $(this).on("click", function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-            console.log("event2: ", e);
-        });
-    });
+    
 });
